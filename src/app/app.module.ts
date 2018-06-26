adf1bfc47388ebcf0f78f94a62544922468898d8
synergy2411 <synergy2411@outlook.com>
2018-06-26 11:03:45 +0530

    structural directive added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
 import { HighlightDirective } from './directives/highlight.directive';
+import { UnlessDirective } from './directives/unless.directive';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     UserComponent,
-    HighlightDirective
+    HighlightDirective,
+    UnlessDirective
   ],
   imports: [
     BrowserModule,
--- src/app/directives/highlight.directive.ts
+++ src/app/directives/highlight.directive.ts
@@ -1,25 +1,37 @@
-import { Directive, ElementRef, Renderer2, Input, HostBinding, HostListener } from '@angular/core';
+import {
+    Directive,
+    ElementRef,
+    Renderer2,
+    Input,
+    HostBinding,
+    HostListener
+} from '@angular/core';
+import { UserComponent } from '../user/user.component';
 
 @Directive({
     selector: '[appHighlight]'
 })
 export class HighlightDirective {
-    @Input('defaultColor') defaultColor: string;
+    @Input('myColor') myColor: string;
+    @Input('defaultColor') defaultColor : string; 
 
-    @HostBinding('style.backgroundColor') bgColor : any;
+    @HostBinding('style.backgroundColor') bgColor: any;
 
-    @HostListener('mouseenter') mouseenter(eventdata){
+    @HostListener('mouseenter') mouseenter(eventdata) {
+        this.bgColor = this.myColor;
+    }
+    @HostListener('mouseleave') mouseleave(eventdata) {
         this.bgColor = this.defaultColor;
-    } 
-    @HostListener('mouseleave') mouseleave(eventdata){
-        this.bgColor = 'transparent';
-    } 
+    }
 
-        constructor(private elementRef: ElementRef, private renderer: Renderer2) {
-            //this.elementRef.nativeElement.style.backgroundColor = "green";
-            // this.renderer.setStyle(this.elementRef.nativeElement,
-            //     "background-color", "aqua");
-            // console.log(this.defaultColor);
-            //
-        }
+    constructor(private elementRef: ElementRef, 
+                    private renderer: Renderer2,
+                    private userComponent : UserComponent) {
+                        //this.userComponent.
+        //this.elementRef.nativeElement.style.backgroundColor = "green";
+        // this.renderer.setStyle(this.elementRef.nativeElement,
+        //     "background-color", "aqua");
+        // console.log(this.defaultColor);
+        this.defaultColor = 'transparent';
+    }
 }
